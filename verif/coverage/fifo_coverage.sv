252a1078c14d628afd1bf9b994a7d860071490b2
Alexander Stoilov <aleksandarstoilov26@gmail.com>
2026-08-17 14:38:03 +0300

    Refactored the logic for we & full and re & empty counters

--- verif/coverage/fifo_coverage.sv
+++ verif/coverage/fifo_coverage.sv
@@ -118,61 +118,68 @@ class fifo_coverage extends uvm_component;
 			@(posedge write_if.clk);
 			if(write_if.rst_n) fifo_cg.sample();
 			// Logic to drive the write && full counters
-			if(write_if.we == 0 && write_if.full == 0) begin
-				we_0_full_0_cntr++;	
-			end
-			else begin	
-				we_0_full_0_cntr = 0;	
-			end
-
-			if(write_if.we == 0 && write_if.full == 1) begin
-				we_0_full_1_cntr++;	
-			end
-			else begin	
-				we_0_full_1_cntr = 0;	
-			end
-
-			if(write_if.we == 1 && write_if.full == 0) begin
-				we_1_full_0_cntr++;	
-			end
-			else begin	
-				we_1_full_0_cntr = 0;	
-			end
-			if(write_if.we == 1 && write_if.full == 0) begin
-				we_1_full_1_cntr++;	
-			end
-			else begin	
-				we_1_full_1_cntr = 0;	
-			end
+			we_full_controller = {write_if.we, write_if.full};
+			case (we_full_controller)
+				2'b00: begin
+					we_0_full_0_cntr++;	
+					we_0_full_1_cntr = 0;
+					we_1_full_0_cntr = 0;	
+					we_1_full_1_cntr = 0;	
+				end
+				2'b01:
+					begin
+						we_0_full_0_cntr = 0;
+						we_0_full_1_cntr++;	
+						we_1_full_0_cntr = 0;	
+						we_1_full_1_cntr = 0;	
+					end
+				2'b10:
+					begin
+						we_0_full_0_cntr = 0;
+						we_0_full_1_cntr = 0;
+						we_1_full_0_cntr++;	
+						we_1_full_1_cntr = 0;		
+					end
+				2'b11:
+					begin
+						we_0_full_0_cntr = 0;
+						we_0_full_1_cntr = 0;
+						we_1_full_0_cntr = 0;	
+						we_1_full_1_cntr++;		
+					end
+			endcase
 
 			// Logic To drive the read && empty counters
-			if(read_if.re == 0 && read_if.empty == 0) begin
-				re_0_empty_0_cntr++;
-			end
-			else begin
-				re_0_empty_0_cntr = 0;
-			end
-
-			if(read_if.re == 0 && read_if.empty == 1) begin
-				re_0_empty_1_cntr++;
-			end
-			else begin
-				re_0_empty_1_cntr = 0;
-			end
-
-			if(read_if.re == 1 && read_if.empty == 0) begin
-				re_1_empty_0_cntr++;
-			end
-			else begin
-				re_1_empty_0_cntr = 0;
-			end
-
-			if(read_if.re == 1 && read_if.empty == 1) begin
-				re_1_empty_1_cntr++;
-			end
-			else begin
-				re_1_empty_1_cntr = 0;
-			end
+			re_empty_controller = {read_if.re, read_if.empty};
+			case (re_empty_controller)
+				2'b00: begin
+					re_0_empty_0_cntr++;	
+					re_0_empty_1_cntr = 0;
+					re_0_empty_1_cntr = 0;	
+					re_1_empty_1_cntr = 0;	
+				end
+				2'b01:
+					begin
+						re_0_empty_0_cntr = 0;
+						re_0_empty_1_cntr++;
+						re_0_empty_1_cntr = 0;	
+						re_1_empty_1_cntr = 0;	
+					end
+				2'b10:
+					begin
+						re_0_empty_0_cntr = 0;
+						re_0_empty_1_cntr = 0;
+						re_0_empty_1_cntr++;	
+						re_1_empty_1_cntr = 0;	
+					end
+				2'b11:
+					begin
+						re_0_empty_0_cntr = 0;
+						re_0_empty_1_cntr = 0;
+						re_0_empty_1_cntr = 0;	
+						re_1_empty_1_cntr++;
+					end
+			endcase
 
 			// Logic to drive we1_re1_emp_0_full_0 counter
 			if (write_if.we == 1 && write_if.full == 0 && read_if.re == 1 && read_if.empty == 0) begin
